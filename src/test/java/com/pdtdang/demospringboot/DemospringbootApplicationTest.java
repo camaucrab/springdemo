95e7de4f88c52ede657ca1dea2233a49275280d3
Phuoc Dang <thanhphuochfs@gmail.com>
2022-11-30 11:05:58 +0700

    testing

---
+++ src/test/java/com/pdtdang/demospringboot/DemospringbootApplicationTest.java
@@ -0,0 +1,12 @@
+package com.pdtdang.demospringboot;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DemospringbootApplicationTest {
+
+    @Test
+    public void main() {
+    }
+}
