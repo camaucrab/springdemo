1bb0c4cd6454f59b10a872f563a074010245d527
Phuoc Dang <thanhphuochfs@gmail.com>
2022-11-30 11:11:30 +0700

    testing

--- src/test/java/com/pdtdang/demospringboot/controller/CustomerControllerTest.java
+++ src/test/java/com/pdtdang/demospringboot/controller/CustomerControllerTest.java
@@ -1,5 +1,7 @@
 package com.pdtdang.demospringboot.controller;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -20,6 +22,34 @@ public class CustomerControllerTest {
     public void shouldReturnDefaultMessage() throws Exception {
 //        this.mockMvc.perform(get("/customer/get-all-customer")).andDo(print()).andExpect(status().isOk())
 //                .andExpect(content().string(containsString("Hello, World")));
+//        System.out.printf("Testing mock Mvc: ",mockMvc.toString());
     }
 
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void getAllCustomer() {
+    }
+
+    @Test
+    public void getCustomer() {
+    }
+
+    @Test
+    public void testGetCustomer() {
+    }
+
+    @Test
+    public void testGetCustomer1() {
+    }
+
+    @Test
+    public void updateCustomer() {
+    }
 }
