1437e1203e686fcb2ae6638aa0446d30f834b300
Phuoc Dang <thanhphuochfs@gmail.com>
2022-12-01 16:55:34 +0700

    test

--- src/test/java/com/pdtdang/demospringboot/DemospringbootApplicationTest.java
+++ src/test/java/com/pdtdang/demospringboot/DemospringbootApplicationTest.java
@@ -2,6 +2,7 @@ package com.pdtdang.demospringboot;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
@@ -14,7 +15,12 @@ import static org.junit.Assert.*;
 @RunWith(SpringRunner.class)
 public class DemospringbootApplicationTest {
 
+    @Autowired
+    DemospringbootApplication demospringbootApplication;
+
     @Test
     public void main() {
+
+        demospringbootApplication.main(new String[] {});
     }
 }
